6f954c2b76bec1fd24db87854858ce5a7c35d49b
manishap1 <manishapawar.net@gmail.com>
2017-09-13 11:13:12 -0500

    function added

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ var port2 = 7500;
 function handleRequest(request,res){
 	res.end("it worked"+ request.url);
 }
+
+function handleRequest2(request,res){
+	res.end("it worked"+ request.url);
+}
 var server = http.createServer(handleRequest);
 server.listen(port,function(){
 	console.log("server listning on: http://localhost:%s"+ port);
 	console.log(port+"Telling Good about Port ");
 
 })
-var server2 = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
 server2.listen(port2,function(){
 	console.log("server listning on: http://localhost:%s"+ port2);
 	console.log(port2+"Telling bad about Port ")
